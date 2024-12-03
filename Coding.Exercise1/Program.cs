18b24157a9930f48f869289207750c1cfb1ec260
AryanCCTech <aryan.yadav@cctech.co.in>
2024-12-03 12:58:53 +0530

    Add project files.

---
+++ Coding.Exercise1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Coding.Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int variable = 10;
+            Console.WriteLine(variable);
+        }
+    }
+}
---
+++ Coding.Exercise2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Coding.Exercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Previously Provided Bugged Code
+            //int num1 = 10;
+            //int num2 = 20;
+            //int num3 = 30;
+            //int average = (num1 + num2 + num3) / 3
+            //Console.WriteLn("The average is: " + average);
+
+            int num1 = 10;
+            int num2 = 20;
+            int num3 = 30;
+            float average = ((num1 + num2 + num3) / 3);
+            Console.WriteLine("The average is: " + average);
+        }
+    }
+}
